e83e0c641a477b51e5f5f1c4d6d5f5da6cfc8b7b
skr305 <2567091600@qq.com>
2022-03-05 15:55:08 +0800

    @chore: add a todo to write the progress

---
+++ src/components/Link.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    data: function() {
+        return {
+
+        };
+    }
+}
+</script>
