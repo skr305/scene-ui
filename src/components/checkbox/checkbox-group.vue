321bf74c1487604f79d7bc1c8bad02608958de29
skr305 <2567091600@qq.com>
2022-04-09 16:46:22 +0800

    @feat: npm resolved

--- src/components/checkbox/checkbox-group.vue
+++ src/components/checkbox/checkbox-group.vue
@@ -15,6 +15,7 @@ export default defineComponent( {
     name: "s-checkbox-group",
     emits: checkBoxGroupEmits,
     props: checkBoxGroupProps,
+    
     setup( props, ctx ) {
         watch( props, () => {
              deps.map( dep => {
